db55bcf70f1020e9ff5d1bcc8980f0991f93b052
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-01 19:53:36 +0000

    move getPost from helper function to middleware

---
+++ mindreadr-backend/src/middleware/getPost.ts
@@ -0,0 +1,22 @@
+import { type Request, type Response, type NextFunction } from 'express'
+
+import db from '../db.js'
+
+// middleware to get the post object from the db
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { key } = req.params
+  if (isNaN(parseInt(key))) {
+    res.sendStatus(400)
+    return
+  }
+
+  const query = {
+    text: 'SELECT * FROM posts WHERE key = $1',
+    values: [key]
+  }
+
+  const result = await db.query(query)
+  if (result.rowCount === 0) res.locals.post = null
+  else res.locals.post = result.rows[0]
+  next()
+}
