127c3b894130de8a1442fab9294c701b9776de62
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-01 15:17:12 +0000

    guard against various user input errors

--- mindreadr-backend/src/routes/auth.ts
+++ mindreadr-backend/src/routes/auth.ts
@@ -4,13 +4,13 @@ import jwt from 'jsonwebtoken'
 
 import verifyUsername from '../middleware/verifyUsername.js'
 import db from '../db.js'
-import authSecret from '../config/auth.js'
+import secret from '../config/auth.js'
 
 const router = Router()
 
 // login as an existing user
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body // test this with blank and no values
+  const { username = '', password = '' } = req.body
   if (username === '' || password === '') {
     res.sendStatus(400)
     return
@@ -20,22 +20,22 @@ router.post('/login', async (req, res) => {
     text: 'SELECT * FROM users WHERE username = $1',
     values: [username]
   }
-
   const result = await db.query(query)
+
   if (result.rowCount === 0) {
     res.sendStatus(404)
     return
   }
 
   const user = result.rows[0]
-  const valid = bcrypt.compareSync(password as string, user.password as string)
+  const valid = bcrypt.compareSync(JSON.stringify(password), user.password as string)
   if (!valid) {
     res.sendStatus(401)
     return
   }
 
   delete user.password
-  const token = jwt.sign({ username: user.username }, authSecret, { expiresIn: 86400 })
+  const token = jwt.sign({ username: user.username }, secret, { expiresIn: 86400 })
   res.send({ token, ...user })
 
   query.text = 'UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE username = $1'
@@ -44,13 +44,13 @@ router.post('/login', async (req, res) => {
 
 // register as a new user
 router.post('/register', verifyUsername, async (req, res) => {
-  const { username, password } = req.body
+  const { username = '', password = '' } = req.body
   if (username === '' || password === '') {
     res.sendStatus(400)
     return
   }
 
-  const hash = bcrypt.hashSync(password as string, 8)
+  const hash = bcrypt.hashSync(JSON.stringify(password), 8)
   const query = {
     text: 'INSERT INTO users VALUES($1, $2)',
     values: [username, hash]
