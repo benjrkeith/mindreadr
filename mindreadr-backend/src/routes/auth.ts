e79598d91da93a0e226aa6b3ab702cd3d4532d01
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-01 12:57:11 +0000

    make eslint happy

--- mindreadr-backend/src/routes/auth.ts
+++ mindreadr-backend/src/routes/auth.ts
@@ -6,13 +6,12 @@ import verifyUsername from '../middleware/verifyUsername.js'
 import db from '../db.js'
 import authSecret from '../config/auth.js'
 
-
 const router = Router()
 
 // login as an existing user
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body
-  if (!username || !password) {
+  const { username, password } = req.body // test this with blank and no values
+  if (username === '' || password === '') {
     res.sendStatus(400)
     return
   }
@@ -22,14 +21,14 @@ router.post('/login', async (req, res) => {
     values: [username]
   }
 
-  let result = await db.query(query)
+  const result = await db.query(query)
   if (result.rowCount === 0) {
     res.sendStatus(404)
     return
   }
 
   const user = result.rows[0]
-  const valid = bcrypt.compareSync(password, user.password)
+  const valid = bcrypt.compareSync(password as string, user.password as string)
   if (!valid) {
     res.sendStatus(401)
     return
@@ -39,19 +38,19 @@ router.post('/login', async (req, res) => {
   const token = jwt.sign({ username: user.username }, authSecret, { expiresIn: 86400 })
   res.send({ token, ...user })
 
-  query.text = `UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE username = $1`
+  query.text = 'UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE username = $1'
   await db.query(query)
 })
 
 // register as a new user
 router.post('/register', verifyUsername, async (req, res) => {
   const { username, password } = req.body
-  if (!username || !password) {
+  if (username === '' || password === '') {
     res.sendStatus(400)
     return
   }
 
-  const hash = bcrypt.hashSync(password, 8)
+  const hash = bcrypt.hashSync(password as string, 8)
   const query = {
     text: 'INSERT INTO users VALUES($1, $2)',
     values: [username, hash]
