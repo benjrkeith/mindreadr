2a2a662ca1766fbe0747673a381d5ee8efd06c1f
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-01 12:42:44 +0000

    comment unused routers

--- mindreadr-backend/src/index.ts
+++ mindreadr-backend/src/index.ts
@@ -2,17 +2,19 @@ import express, { json } from 'express'
 import logger from 'morgan'
 
 import authRouter from './routes/auth.js'
-import postsRouter from './routes/posts.js'
+// import postsRouter from './routes/posts.js'
+// import usersRouter from './routes/users.js'
 
 const app = express()
 
 app.use(json())
 app.use(logger('dev'))
 
-app.get('/', (req, res) => {res.send('hello there!')})
+app.get('/', (req, res) => { res.send('hello there!') })
 app.use('/api/auth', authRouter)
-app.use('/api/posts', postsRouter)
+// app.use('/api/posts', postsRouter)
+// app.use('/api/users', usersRouter)
 
 app.listen(4000, () => {
-    console.log('Server is running at http://localhost:4000')
+  console.log('Server is running at http://localhost:4000')
 })
