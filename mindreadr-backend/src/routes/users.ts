3db9b8183df1839144682929af36b6b4c6f8d97c
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-05 07:23:01 +0000

    add route for getting user details and their likes

--- mindreadr-backend/src/routes/users.ts
+++ mindreadr-backend/src/routes/users.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import db from '../db.js'
 import verifyToken from '../middleware/verifyToken.js'
 import parseLimits from '../middleware/parseLimits.js'
+import getUser from '../middleware/getUser.js'
 
 const router = Router()
 
@@ -21,4 +22,25 @@ router.get('/', parseLimits, async (req, res) => {
   res.send(result.rows)
 })
 
+router.get('/:username', getUser, async (req, res) => {
+  const user = res.locals.targetUser
+  res.send(user)
+})
+
+router.get('/:username/likes', getUser, async (req, res) => {
+  const user = res.locals.targetUser
+  const query = {
+    text: `SELECT key, author, content, parent, posts.created_at
+      FROM likes JOIN posts on post = key WHERE author = $1`,
+    values: [user.username]
+  }
+
+  const result = await db.query(query)
+  res.send(result.rows.map(x => x))
+})
+
 export default router
+
+// array(SELECT username FROM followers WHERE follower = $1) as following,
+// array(SELECT follower FROM followers WHERE username = $1) as followers,
+// array(SELECT post FROM likes WHERE username = $1) as likes
