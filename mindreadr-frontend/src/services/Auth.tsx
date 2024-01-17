5fc70e07d0422b1c33cf5f796d248454960353a7
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-17 07:09:08 +0000

    add auth helper functions for managing sessionStorage

---
+++ mindreadr-frontend/src/services/Auth.tsx
@@ -0,0 +1,22 @@
+import { type User, defaultUser } from '../App'
+
+interface Header {
+  headers: {
+    token: string
+  }
+}
+
+export function logOut (): void {
+  sessionStorage.removeItem('user')
+}
+
+export function acquireUser (): User {
+  const cache = sessionStorage.getItem('user')
+
+  if (cache !== null) { return JSON.parse(cache) } else { return defaultUser }
+}
+
+export function header (): Header {
+  const user = acquireUser()
+  return { headers: { token: user?.token } }
+}
