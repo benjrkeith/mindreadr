17051bc355699aaaae5fd20efb05dc286a493f73
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-20 20:55:35 +0000

    display all loaded posts instead of just one

--- mindreadr-frontend/src/pages/Feed.tsx
+++ mindreadr-frontend/src/pages/Feed.tsx
@@ -4,18 +4,18 @@ import { getPosts, type RawPost } from '../api/getPosts'
 import Post from '../components/Post'
 
 export default function Users (): ReactElement {
-  const [posts, setPosts] = useState<RawPost>([])
+  const [posts, setPosts] = useState<RawPost[]>([])
 
   const loadMore = useCallback(async () => {
     const next = await getPosts()
-    setPosts(next[0])
+    setPosts(next)
   }, [])
 
   useEffect(() => { void loadMore() }, [loadMore])
 
   return (
     <div className='feed-container'>
-      <Post data={posts}/>
+      {posts.map(data => <Post key={data.key} data={data}/>)}
     </div>
   )
 }
