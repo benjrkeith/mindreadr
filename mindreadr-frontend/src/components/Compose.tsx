4f98160a8f59535dbd6e8d282d93b2c10f7e9e09
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-01-30 22:37:20 +0000

    allow selecting a specific user using arrow keys when tagging someone - also enforce 140 char limit

--- mindreadr-frontend/src/components/Compose.tsx
+++ mindreadr-frontend/src/components/Compose.tsx
@@ -8,6 +8,7 @@ export default function Compose (): ReactElement {
   const [users, setUsers] = useState<string[]>([])
   const [filtered, setFiltered] = useState<string[]>([])
   const [offset, setOffset] = useState<number>(0)
+  const [targetUser, setTargetUser] = useState<number>(0)
 
   const inputRef = React.useRef<HTMLDivElement>(null)
 
@@ -67,12 +68,32 @@ export default function Compose (): ReactElement {
 
     if (match === null) {
       setFiltered([])
+      setTargetUser(0)
     } else {
       setFiltered(users.filter((u) => u.startsWith(match[0].replace(' @', ''))))
     }
   }
 
   function handleKeyDown (e: React.KeyboardEvent): void {
+    const textInput = inputRef.current
+    if (textInput === null) return
+
+    if (textInput.innerText.length >= 140 && e.key !== 'Backspace') {
+      // display error char limit reached
+      e.preventDefault()
+      return
+    }
+
+    if (e.key === 'ArrowDown') {
+      e.preventDefault()
+      if (targetUser < filtered.length - 1) setTargetUser(prev => prev + 1)
+      return
+    } else if (e.key === 'ArrowUp') {
+      e.preventDefault()
+      if (targetUser > 0) setTargetUser(prev => prev - 1)
+      return
+    }
+
     if (e.key === 'Enter') e.preventDefault()
     if (e.key !== 'Enter' || filtered.length === 0) return
 
@@ -80,10 +101,11 @@ export default function Compose (): ReactElement {
     const match = newInput.match(re)
     if (match === null) return
 
-    newInput = newInput.replace(re, ` <span class='blue'>@${filtered[0]}</span>\u200B`)
+    newInput = newInput.replace(re, ` <span class='blue'>@${filtered[targetUser]}</span>\u200B`)
     setInput(newInput)
-    setOffset(prev => prev + filtered[0].length - match[0].length + 4)
+    setOffset(prev => prev + filtered[targetUser].length - match[0].length + 4)
     setFiltered([])
+    setTargetUser(0)
   }
 
   return (
@@ -91,7 +113,9 @@ export default function Compose (): ReactElement {
         <div className='compose-input' contentEditable onKeyDown={handleKeyDown} onInput={handleInput}
           suppressContentEditableWarning dangerouslySetInnerHTML={{ __html: input }} ref={inputRef}/>
         <div className='compose-search-container'>
-          {filtered.map(i => <span key={i} className='compose-search-item'>{i}</span>)}
+          {filtered.map((user, i) => <span key={user} className={i === targetUser
+            ? 'compose-search-target'
+            : 'compose-search-item'}>{user}</span>)}
         </div>
     </div>
   )
