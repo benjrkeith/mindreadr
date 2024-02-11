f9340fac6e2061e58e08a22d7023e4609b3ec2ee
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-02-11 08:45:05 +0000

    count total replies and reposts when getting posts from db

--- mindreadr-backend/src/routes/posts.ts
+++ mindreadr-backend/src/routes/posts.ts
@@ -29,7 +29,9 @@ router.get('/', parseLimits, getFollowers, async (req, res) => {
     text: `SELECT key, author, content, created_at, parent,
       (SELECT author AS parent_author FROM POSTS AS s WHERE s.key=p.parent),
       EXISTS(SELECT * FROM posts AS n WHERE n.author=$3 AND n.parent=p.key AND n.content=p.content) AS reposted,
+      (SELECT COUNT(*) AS reposts FROM posts WHERE parent=key AND content=p.content),
       EXISTS(SELECT * FROM posts AS n WHERE n.author=$3 AND n.parent=p.key AND NOT n.content=p.content) AS replied,
+      (SELECT COUNT(*) AS replies FROM posts WHERE parent=p.key AND NOT content=p.content),
       (SELECT COUNT(*) AS likes FROM likes WHERE post=key), 
       EXISTS(SELECT * FROM likes WHERE username=$3 AND post=key) AS liked 
       FROM posts AS p WHERE ($4::text IS NULL AND ($6 IS FALSE OR author=ANY($5)) 
