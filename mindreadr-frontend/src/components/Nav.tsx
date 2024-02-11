2de0abd0d2545a7af87f014ad86a8d278aa64ec4
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-02-11 08:36:54 +0000

    show indicatior on nav bar of which page you are currently on

--- mindreadr-frontend/src/components/Nav.tsx
+++ mindreadr-frontend/src/components/Nav.tsx
@@ -1,21 +1,28 @@
 import React, { type ReactElement } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Nav (): ReactElement {
+  const loc = useLocation()
+  console.log(loc)
+
   return (
-    <div className='grid grid-cols-4 justify-items-center h-[10%] box-border border-t-2 border-white
+    <div className='grid grid-cols-4 justify-items-center items-center h-[10%] box-border border-t-2 border-white
           border-double w-full p-2'>
-        <Link to='/feed' className='min-h-0 h-full'>
-          <img src='home.png' alt='' className='min-h-0 max-h-full object-cover filter-white' />
+        <Link to='/feed' className='min-h-0 h-4/6'>
+          <img src='home.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+            ${loc.pathname === '/feed' ? 'filter-purple' : ''}`} />
         </Link>
-        <Link to='/trending' className='min-h-0 h-full'>
-          <img src='trends.png' alt='' className='min-h-0 max-h-full object-cover filter-white' />
+        <Link to='/trending' className='min-h-0 h-4/6'>
+          <img src='trends.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+            ${loc.pathname === '/trending' ? 'filter-purple' : ''}`} />
         </Link>
-        <Link to='/users' className='min-h-0 h-full'>
-          <img src='users.png' alt='' className='min-h-0 max-h-full object-cover filter-white' />
+        <Link to='/users' className='min-h-0 h-4/6'>
+          <img src='users.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+            ${loc.pathname === '/users' ? 'filter-purple' : ''}`} />
         </Link>
-        <Link to='/inbox' className='min-h-0 h-full'>
-          <img src='settings.png' alt='' className='min-h-0 max-h-full object-cover filter-white' />
+        <Link to='/inbox' className='min-h-0 h-4/6'>
+          <img src='settings.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+            ${loc.pathname === '/inbox' ? 'filter-purple' : ''}`} />
         </Link>
     </div>
   )
