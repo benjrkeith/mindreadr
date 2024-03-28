412b54489575fe244eebc0cba0f5eea3ffcc97e7
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-03-28 10:52:27 +0000

    add inbox page and conversation preview component

---
+++ mindreadr-frontend/src/components/ConversationPreview.tsx
@@ -0,0 +1,42 @@
+import React, { type ReactElement } from 'react'
+import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
+
+interface Props {
+  i: number
+  conversation: {
+    conversation: number
+    users: string[]
+    avatar: string
+    created_at: string
+    content: string
+  }
+}
+
+export default function ConversationPreview (props: Props): ReactElement {
+  const { conversation: conv, i } = props
+  const user = useAuth()
+
+  const getTitle = (users: string[]): string => {
+    users.splice(users.indexOf(user.username), 1)
+    return users.join(', ')
+  }
+
+  return (
+    <Link key={i} className={`flex p-3 ${i % 2 === 0 ? 'bg-zinc-900' : ''}`}
+          to={`/inbox/${conv.conversation}`}>
+          <img src={conv.avatar} alt='avatar' className='h-16 rounded-full'/>
+          <div className='w-full px-2 flex flex-col my-auto'>
+            <div className='flex w-full'>
+                <h1 className='text-xl font-semibold w-full'>
+                    {getTitle(conv.users)}
+                </h1>
+                <p className='text-xs my-auto w-full text-end'>
+                    {new Date(conv.created_at).toLocaleString().substring(0, 17)}
+                </p>
+            </div>
+            <p className='text-sm'>{conv.content}</p>
+          </div>
+        </Link>
+  )
+}
--- mindreadr-frontend/src/pages/Inbox.tsx
+++ mindreadr-frontend/src/pages/Inbox.tsx
@@ -1,9 +1,24 @@
-import React, { type ReactElement } from 'react'
+import React, { type ReactElement, useCallback, useEffect, useState } from 'react'
+
+import getInbox, { type Conversation } from '../api/getInbox'
+import TitleBar from '../components/TitleBar'
+import ConversationPreview from '../components/ConversationPreview'
 
 export default function Inbox (): ReactElement {
+  const [conversations, setConversations] = useState<Conversation[]>([])
+
+  const loadInbox = useCallback(async () => {
+    const res = await getInbox()
+    setConversations(res)
+  }, [getInbox])
+
+  useEffect(() => { void loadInbox() }, [loadInbox])
+
   return (
     <div className='h-[90%] flex flex-col overflow-scroll'>
-        Inbox
+      <TitleBar title='Inbox' btnText='New Message' btnCallback={() => { console.log('test') }}/>
+      {conversations.map((conv, i) =>
+        <ConversationPreview key={i} conversation={conv} i={i}/>)}
     </div>
   )
 }
