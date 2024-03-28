b7dfd673bf1059c46fc95aff700b8689a0ee9059
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-03-28 09:50:02 +0000

    add inbox url to api common imports

--- mindreadr-frontend/src/api/common.tsx
+++ mindreadr-frontend/src/api/common.tsx
@@ -2,3 +2,4 @@ export const API_URL = '/api'
 export const AUTH_URL = `${API_URL}/auth`
 export const POSTS_URL = `${API_URL}/posts`
 export const USERS_URL = `${API_URL}/users`
+export const INBOX_URL = `${API_URL}/inbox`
