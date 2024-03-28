3c39ceb55d6574bd3e6f9e40eb842fa1c59817ed
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-03-28 10:51:27 +0000

    update title bar component add dynamic button

--- mindreadr-frontend/src/components/TitleBar.tsx
+++ mindreadr-frontend/src/components/TitleBar.tsx
@@ -4,16 +4,22 @@ import { Link } from 'react-router-dom'
 interface Props {
   title: string
   link?: string
+  btnText?: string
+  btnCallback?: () => void
 }
 
-export default function TitleBar ({ title, link }: Props): ReactElement {
+export default function TitleBar (props: Props): ReactElement {
+  const { title, link, btnText, btnCallback } = props
+
   return (
     <div className='flex sticky top-0 bg-grey'>
         {link !== undefined && <Link to={link} className='font-semibold
-            text-4xl my-auto pl-3'>&#8592;</Link>}
+            text-4xl my-auto pl-3 text-purple-600'>&#8592;</Link>}
         <h1 className='text-white text-4xl p-3 leading-7 font-semibold'>
           {title}
         </h1>
+        {btnText !== undefined && <button onClick={btnCallback} className='
+          text-purple-600 grow font-semibold text-end p-3'>{btnText}</button>}
     </div>
   )
 }
