18be8c3447f473737d35a73eea4322358b93c371
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-03-28 11:01:01 +0000

    move nav icons to assets and import properly

--- mindreadr-frontend/src/components/Nav.tsx
+++ mindreadr-frontend/src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import React, { type ReactElement } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
+import feed from '../assets/home.png'
+import trending from '../assets/trends.png'
+import users from '../assets/users.png'
+import inbox from '../assets/inbox.png'
+
 export default function Nav (): ReactElement {
   const loc = useLocation()
 
@@ -8,19 +13,19 @@ export default function Nav (): ReactElement {
     <div className='grid grid-cols-4 justify-items-center items-center h-[10%] box-border border-t-2 border-white
           border-double w-full p-2'>
         <Link to='/feed' className='min-h-0 h-4/6'>
-          <img src='home.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+          <img src={feed} alt='' className={`min-h-0 max-h-full object-cover filter-white 
             ${loc.pathname === '/feed' ? 'filter-purple' : ''}`} />
         </Link>
         <Link to='/trending' className='min-h-0 h-4/6'>
-          <img src='trends.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+          <img src={trending} alt='' className={`min-h-0 max-h-full object-cover filter-white 
             ${loc.pathname === '/trending' ? 'filter-purple' : ''}`} />
         </Link>
         <Link to='/users' className='min-h-0 h-4/6'>
-          <img src='users.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+          <img src={users} alt='' className={`min-h-0 max-h-full object-cover filter-white 
             ${loc.pathname === '/users' ? 'filter-purple' : ''}`} />
         </Link>
         <Link to='/inbox' className='min-h-0 h-4/6'>
-          <img src='settings.png' alt='' className={`min-h-0 max-h-full object-cover filter-white 
+          <img src={inbox} alt='' className={`min-h-0 max-h-full object-cover filter-white 
             ${loc.pathname === '/inbox' ? 'filter-purple' : ''}`} />
         </Link>
     </div>
