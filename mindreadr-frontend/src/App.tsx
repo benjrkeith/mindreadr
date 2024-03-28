ce48f75fd107a238cbcd9477b53646edcbfb807e
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-03-28 09:47:40 +0000

    tidy up main app component

--- mindreadr-frontend/src/App.tsx
+++ mindreadr-frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useState, type ReactElement, createContext } from 'react'
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
 
+import Conversation from './pages/Conversation'
 import Feed from './pages/Feed'
 import Inbox from './pages/Inbox'
 import LogIn from './pages/Login'
 import Register from './pages/Register'
 import Trending from './pages/Trending'
 import Users from './pages/Users'
+
 import Auth from './components/Auth'
 import Nav from './components/Nav'
 import Protected from './components/Protected'
+
 import './App.css'
 
 export interface User {
@@ -23,7 +26,13 @@ export interface User {
   avatar: string
 }
 
-export const defaultUser: User = { username: '', token: '', created_at: '', last_login: '', avatar: '' }
+export const defaultUser: User = {
+  username: '',
+  token: '',
+  created_at: '',
+  last_login: '',
+  avatar: ''
+}
 export const userCtx = createContext(defaultUser)
 
 export default function App (): ReactElement {
@@ -37,11 +46,24 @@ export default function App (): ReactElement {
           <Route path='/login' element={
             <Auth setUser={setUser} el={<LogIn setUser={setUser}/>}/>
           }/>
-          <Route path='/register' element={<Auth setUser={setUser} el={<Register/>}/>}/>
-          <Route path='/feed' element={<Protected el={<Feed/>}/>}/>
-          <Route path='/trending' element={<Protected el={<Trending/>}/>}/>
-          <Route path='/users/*' element={<Protected el={<Users/>}/>}/>
-          <Route path='/inbox' element={<Protected el={<Inbox/>}/>} />
+          <Route path='/register' element={
+            <Auth setUser={setUser} el={<Register/>}/>
+          }/>
+          <Route path='/feed' element={
+            <Protected el={<Feed/>}/>
+          }/>
+          <Route path='/trending' element={
+            <Protected el={<Trending/>}/>
+          }/>
+          <Route path='/users/*' element={
+            <Protected el={<Users/>}/>
+          }/>
+          <Route path='/inbox' element={
+            <Protected el={<Inbox/>}/>
+          }/>
+          <Route path='/inbox/:conversation' element={
+            <Protected el={<Conversation/>}/>
+          }/>
         </Routes>
         {user.token === '' ? <></> : <Nav/>}
       </BrowserRouter>
