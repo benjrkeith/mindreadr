2902da6e93c7b5239edc21fdd47b1313fdd26ba1
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-05-23 09:11:11 +0100

    remove express backend ready to migrate to NestJS

--- mindreadr-backend/src/middleware/checkJSON.ts
+++
@@ -1,7 +0,0 @@
-import { type Request, type Response, type NextFunction as NF } from 'express'
-
-// basic error handling to verify all bodys recieved are json
-export default (err: Error, req: Request, res: Response, next: NF): void => {
-  if (err instanceof SyntaxError) res.status(400)
-  else next()
-}
