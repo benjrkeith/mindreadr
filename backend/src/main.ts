84b588b09040d252aaed25fe66edda8d11b0f5ce
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-06-09 09:13:52 +0100

    Enable cors for non local host clients

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from 'src/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://192.168.0.200:5173'],
+  })
   app.setGlobalPrefix('api')
   app.useGlobalPipes(
     new ValidationPipe({
