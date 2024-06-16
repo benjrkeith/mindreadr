a25035eb9964879755fcc3ad0293fbbd4ed6aedf
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-06-16 10:44:38 +0100

    Select user avatars when getting list of chat members

--- backend/src/chat/service/chat.service.ts
+++ backend/src/chat/service/chat.service.ts
@@ -69,6 +69,7 @@ export class ChatService {
               select: {
                 id: true,
                 username: true,
+                avatar: true,
               },
             },
           },
