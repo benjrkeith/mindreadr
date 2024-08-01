4570a9696f7a12077d1d37cbb0ddd3eacafa7081
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-08-01 21:01:03 +0100

    Update and optimise Infinite Scroll component

---
+++ frontend/src/common/components/JumpDown.tsx
@@ -0,0 +1,17 @@
+interface JumpDownProps {
+  callback: (bhvr: ScrollBehavior) => void
+}
+
+export function JumpDown({ callback }: JumpDownProps) {
+  return (
+    <div className='absolute bottom-10 w-full text-center'>
+      <button
+        onClick={() => callback('smooth')}
+        className='size-10 rounded-full bg-dark_bg_1dp text-4xl font-bold 
+        shadow-[0px_-0px_8px] shadow-black/50 hover:bg-dark_bg_2dp'
+      >
+        🡳
+      </button>
+    </div>
+  )
+}
