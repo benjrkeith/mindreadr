a8665c2d5f2f327e37ea330828885958f2464ae3
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-08-04 20:12:15 +0100

    Implement default avatars using custom element rather than alt text, styling is more consistent this way

--- frontend/src/common/components/Avatar.tsx
+++ frontend/src/common/components/Avatar.tsx
@@ -9,17 +9,31 @@ interface AvatarProps {
 export function Avatar({ user, sx }: AvatarProps) {
   const { color, useBlackText } = getUserColor(user.username)
 
-  return (
-    <img
-      src={user.avatar}
-      alt={user.username.substring(0, 1)}
-      style={{ backgroundColor: color }}
-      className={cls(
-        `flex aspect-square items-center justify-center object-cover text-3xl 
-        font-semibold uppercase shadow-[0px_0px_10px] shadow-black/20`,
-        sx,
-        { 'text-black': useBlackText },
-      )}
-    />
-  )
+  if (user.avatar)
+    return (
+      <img
+        src={user.avatar}
+        className={cls(
+          'aspect-square object-cover shadow-[0px_0px_10px] shadow-black/20',
+          sx,
+        )}
+      />
+    )
+  else
+    return (
+      <div
+        style={{ backgroundColor: color }}
+        className={cls(
+          'flex aspect-square text-3xl shadow-[0px_0px_10px] shadow-black/20',
+          sx,
+          {
+            'text-black': useBlackText,
+          },
+        )}
+      >
+        <span className={cls('m-auto font-medium uppercase')}>
+          {user.username.substring(0, 1)}
+        </span>
+      </div>
+    )
 }
