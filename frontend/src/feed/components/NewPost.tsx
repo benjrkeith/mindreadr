0b83bd0251ae8bcc1ada4073a676a576806b0309
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2024-08-10 18:55:53 +0100

    Improvements to tag detection and completion

--- frontend/src/feed/components/NewPost.tsx
+++ frontend/src/feed/components/NewPost.tsx
@@ -13,8 +13,9 @@ export function NewPost() {
   // get all usernames so we can search through them when tagging a user
   const usersQuery = useQuery({ queryKey: ['users'], queryFn: getAllUsernames })
 
-  const tags = useMemo(
-    () => getTags(content, usersQuery.data),
+  // gets tag autocompletion data
+  let tags = useMemo(
+    () => getTags(content, ref.current?.selectionStart, usersQuery.data),
     [content, usersQuery.data],
   )
 
@@ -41,9 +42,8 @@ export function NewPost() {
       const cn = 'bg-blue-800 px-1 translate-y-full absolute'
       const span = `<span class='${cn}'>${tags.results[selected].username}</span>`
 
-      const offset = tags.match.index
-      const before = result.substring(0, offset + 1)
-      const after = result.substring(offset + 1)
+      const before = result.substring(0, tags.match.index + 1)
+      const after = result.substring(tags.match.index + 1)
       result = `${before}${span}${after}`
     }
 
@@ -65,25 +65,22 @@ export function NewPost() {
 
     switch (e.key) {
       case 'Backspace': {
-        if ([' ', ',', '.'].includes(target.value.charAt(offset - 1))) return
+        RESOLVED_TAG_RE.lastIndex = 0
+        let resolved = RESOLVED_TAG_RE.exec(target.value)
+        while (resolved !== null) {
+          if (
+            resolved.index < offset &&
+            resolved.index + resolved[0].length >= offset
+          )
+            break
+          resolved = RESOLVED_TAG_RE.exec(target.value)
+        }
 
-        // find which word the user pressed back space in
-        let total = 0
-        const words = target.value.split(' ')
-        const word = words.find((part) => {
-          total += part.length + 1
-          return total > offset
-        })
-
-        if (!word || !word.match(RESOLVED_TAG_RE)) return
+        if (resolved === null) return
         e.preventDefault()
 
-        const idx = words.indexOf(word)
-        words[idx - 1] += ' '
-        words.splice(idx, 1)
-
-        setContent(words.join(' '))
-        setFixedOffset(total - word.length - 1)
+        setContent(target.value.replace(resolved[0], ''))
+        setFixedOffset(resolved.index)
         break
       }
       case 'Enter': {
@@ -91,10 +88,14 @@ export function NewPost() {
         if (tags === undefined) return
 
         const username = tags.results[selected].username
-        setContent(content.replace(tags.match[0], `@\u200b${username}`))
-        setFixedOffset(offset - tags.match.length + username.length + 1)
+        const before = content.substring(0, tags.match.index)
+        const after = content.substring(tags.match.index + tags.match[0].length)
+
+        setContent(`${before}@\u200b${username}\u200b${after}`)
+        setFixedOffset(tags.match.index + username.length + 3)
         break
       }
+      // up and down are for autocompletion when tagging a user
       case 'ArrowUp': {
         if (tags === undefined) return
         e.preventDefault()
@@ -109,6 +110,37 @@ export function NewPost() {
         if (selected < tags.results.length - 1) setSelected(selected + 1)
         break
       }
+      // left and right allow navigating normally without the ZWS interfering
+      case 'ArrowLeft': {
+        if (ref.current === null) return
+        const offset = ref.current.selectionStart
+
+        if (content.charAt(offset - 1) === '\u200b')
+          ref.current.setSelectionRange(offset - 1, offset - 1)
+        break
+      }
+      case 'ArrowRight': {
+        if (ref.current === null) return
+        const offset = ref.current.selectionStart
+
+        if (content.charAt(offset + 1) === '\u200b')
+          ref.current.setSelectionRange(offset + 1, offset + 1)
+        break
+      }
+      default: {
+        RESOLVED_TAG_RE.lastIndex = 0
+        let resolved = RESOLVED_TAG_RE.exec(content)
+        while (resolved !== null) {
+          if (
+            resolved.index < offset &&
+            resolved.index + resolved[0].length > offset
+          )
+            break
+          resolved = RESOLVED_TAG_RE.exec(content)
+        }
+
+        if (resolved !== null) e.preventDefault()
+      }
     }
   }
 
@@ -121,14 +153,14 @@ export function NewPost() {
         value={content}
         onInput={(e) => setContent(e.currentTarget.value)}
         onKeyDown={handleKeyDown}
-        className='hide-text break-word z-10 col-[1/1] row-[1/1] h-full w-full 
-        resize-none bg-transparent'
+        className='hide-text z-10 col-[1/1] row-[1/1] h-full w-full resize-none 
+        break-words bg-transparent'
       />
 
       <span
         dangerouslySetInnerHTML={{ __html: getFormattedContent() }}
-        className='break-word col-[1/1] row-[1/1] h-fit w-full whitespace-pre 
-        text-wrap'
+        className='col-[1/1] row-[1/1] h-full w-full whitespace-pre text-wrap 
+        break-words'
       />
     </div>
   )
--- frontend/src/feed/methods/getTags.ts
+++ frontend/src/feed/methods/getTags.ts
@@ -1,18 +1,31 @@
 import { types } from 'src/common'
 
-export const UNRESOLVED_TAG_RE = /@[\w\d]+/
-export const RESOLVED_TAG_RE = /@\u200b[\w\d]+/g
+export const UNRESOLVED_TAG_RE = /@[\w\d]+/g
+export const RESOLVED_TAG_RE = /@\u200b[\w\d]+\u200b/g
 
-export const getTags = (content: string, data: types.User[] = []) => {
-  const matches = UNRESOLVED_TAG_RE.exec(content)
-  if (matches === null) return undefined
+export const getTags = (
+  _content: string,
+  offset: number = 0,
+  data: types.User[] = [],
+) => {
+  const content = _content.substring(0, offset)
 
+  UNRESOLVED_TAG_RE.lastIndex = 0
+  let match = UNRESOLVED_TAG_RE.exec(content)
+  while (match !== null) {
+    if (match.index < offset && match.index + match[0].length >= offset) break
+    match = UNRESOLVED_TAG_RE.exec(content)
+  }
+
+  if (match === null) return undefined
   const users = data.filter(
     (user) =>
-      user.username.startsWith(matches[0].replace('@', '')) &&
-      !content.match(RESOLVED_TAG_RE)?.includes(`@\u200b${user.username}`),
+      user.username.startsWith(match[0].replace('@', '')) &&
+      !content
+        .match(RESOLVED_TAG_RE)
+        ?.includes(`@\u200b${user.username}\u200b`),
   )
 
   if (users.length === 0) return undefined
-  return { match: matches, results: users }
+  return { match, results: users }
 }
